2e73ee98260f71e847b30836085ad890c8793996
Nelabh Kotiya <nelabhkotiya@gmail.com>
2015-11-12 23:56:37 +0530

    Created view for buyer

---
+++ app/Http/Controllers/BDController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+use Views;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+
+ class BDController extends BaseController
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    public function home()
+    {
+    	return \View::make('wel');
+    }
+}
---
+++ resources/views/wel.blade.php
@@ -0,0 +1,242 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="">
+<meta name="author" content="">
+<title>form</title>
+<!-- Bootstrap core CSS -->
+<link href="{{URL::asset('css/bootstrap.css')}}" rel="stylesheet"/>
+<link href="{{URL::asset('css/style.css')}}" rel="stylesheet" type="text/css" />
+<link href="{{URL::asset('font-awesome/css/font-awesome.min.css')}}" rel="stylesheet" type="text/css"/>
+</head>
+<body>
+<nav class="nav navbar-default navbar nav_wrap">
+<div class="container">
+	<div class="row">
+		<div class="col-md-12 nav_outer">
+			<div class="col-md-2 logo_outer">
+			<img src="{{URL::asset('img/logo.png')}}" class="img-responsive">
+			</div>
+			<div class="col-md-7 search_barouter">
+			<div class="col-md-12 input-group inputdiv">
+      <input type="text" class="col-md-12 form-control" placeholder="Search for...">
+			</div>
+			</div>
+			<div class="col-md-3 btnlogin_outer">
+			<div class="col-md-6 col-md-offset-1"><div class="col-md-9 btn btn-default headbtn">Log in</div></div>
+			<div class="col-md-5 signouter"><div class="col-md-12 btn btn-default headbtn">Sign Up</div></div>
+			</div>
+			</div>
+			</div>
+			</div>
+			</nav>
+			<div class="container-fluid">
+		<div class="row">
+		<div class="col-md-12 getbutton_outer">
+		<div class="col-md-10 getbutton">
+		</div>
+			<div class="col-md-2 getbutton">
+				<button type="button" class="col-md-10 col-md-offset-2 get_quotations">Get Quotations</button>
+		</div>
+		</div>
+		</div>
+		</div>
+			<div class="container">
+		<div class="row">
+		<div class="col-md-12 schedule_formouter">
+		<div class="col-md-12 schedule_form">
+      <div class="col-md-12 table-responsive">
+        <table summary="" class="table table-bordered table-hover">
+		<caption><h3>Schedule Purchase</h3></caption>
+          <thead>
+            <tr class="headbg">
+              <th>Product	</th>
+              <th>Product Description</th>
+              <th>Quantity</th>
+              <th>Purchase Frequency</th>
+              <th> Sample Required</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Oak Wood Dinner set</td>
+              <td>used Fine material</td>
+              <td>10</td>
+              <td></td>
+              <td><input type=
+			  "Checkbox"</td>
+            </tr>
+           <tr>
+              <td>Oak Wood Dinner set</td>
+              <td>used Fine material</td>
+              <td>10</td>
+              <td></td>
+              <td><input type=
+			  "Checkbox"</td>
+            </tr>
+            <tr>
+              <td>Oak Wood Dinner set</td>
+              <td>used Fine material</td>
+              <td>10</td>
+              <td></td>
+              <td><input type=
+			  "Checkbox"</td>
+            </tr>
+           <tr>
+              <td>Oak Wood Dinner set</td>
+              <td>used Fine material</td>
+              <td>10</td>
+              <td></td>
+              <td><input type=
+			  "Checkbox"</td>
+            </tr>
+            <tr>
+              <td>Oak Wood Dinner set</td>
+              <td>used Fine material</td>
+              <td>10</td>
+              <td></td>
+              <td><input type=
+			  "Checkbox"</td>
+            </tr>
+          </tbody>
+          <tfoot>
+       
+          </tfoot>
+        </table>
+		</div>
+		</div>
+		</div>
+		</div>
+      </div><!--end of .table-responsive-->
+	  			
+		<div class="container">
+		<div class="row">
+		<div class="col-md-10 col-md-offset-1 tablebotbtn_formouter">
+			<div class="col-md-10 tablebotbtn_forminner">
+			<div class="btn btn-info">
+		<span>+</span>Add Another Field
+		</div>
+		</div>
+			<div class="col-md-2 tablebotbtn_formoutersumbtn">
+			<div class="col-md-10 col-md-offset-2 btn btn-default submitbtnform">
+		SUBMIT
+		</div>
+		</div>
+		</div>
+		</div>
+		</div>
+			<div class="footer_wrap">
+			<div class="container">
+		<div class="row">
+		<div class="col-md-12 footer_outer">
+		<div class="col-md-4 footer_left">
+		<h3>About Vklup</h3>
+		<div class="col-md-5 footer_border">
+		<hr>
+		</div>
+		<p class="col-md-12 footerp">Vkulp is here to ensure an easy & efficient 
+procurement process for hospitality industry. </p>
+		<div class="col-md-12 footiconouter">
+		<div class="col-md-2 socialicons">
+       <a href="#"><i class="fa fa-facebook faicon"></i></a>
+	   </div>
+	   <div class="col-md-2 socialicons">
+       <a href="#"><i class="fa  fa-twitter faicon"></i></a>
+	   </div>
+	   <div class="col-md-2 socialicons">
+       <a href="#"><i class="fa fa-linkedin faicon"></i></a>
+	   </div>
+	   <div class="col-md-2 socialicons">
+       <a href="#"><i class="fa fa-youtube faicon"></i></a>
+	   </div>
+	   </div>
+		</div>
+		<div class="col-md-4 footer_mid">
+		<h3>News Letter</h3>
+		<div class="col-md-5 footer_border">
+		<hr>
+		</div>
+				<p class="col-md-12 footerp">Vkulp is here to ensure an easy & efficient 
+procurement process for hospitality industry. </p>
+		<div class="col-md-8 footsearch">
+			<div class="col-md-12 input-group footsearch_barouter">
+      <input type="text" class="col-md-12 form-control" placeholder="Search for...">
+			</div>
+					<div class="col-md-12 submitbtn">
+			<div class="col-md-7 col-md-offset-5 btn btn-default submitbtninner">SUBMIT</div>
+			</div>
+			</div>
+		</div>
+		<div class="col-md-4 footer_right">
+		<h3>Quick Link</h3>
+		<div class="col-md-5 footer_border">
+		<hr>
+		</div>
+		<div class="col-md-12 sociallink_outer">
+		<div class="col-md-1 sociallink_outer">
+	<img src="{{URL::asset('img/arrow.png')}}">
+	</div>
+		<div class="col-md-10 sociallink_outertxt">
+	<a href="#">Home</a>
+	</div>
+	</div>
+		<div class="col-md-12 sociallink_outer">
+		<hr>
+		<div class="col-md-1 sociallink_outer">
+	<img src="{{URL::asset('img/arrow.png')}}">
+	</div>
+		<div class="col-md-10 sociallink_outertxt">
+	<a href="#">About</a>
+	</div>
+	</div>
+		<div class="col-md-12 sociallink_outer">
+		<hr>
+		<div class="col-md-1 sociallink_outer">
+	<img src="{{URL::asset('img/arrow.png')}}">
+	</div>
+		<div class="col-md-10 sociallink_outertxt">
+	<a href="#">Blog</a>
+	</div>
+	</div>
+		<div class="col-md-12 sociallink_outer">
+		<hr>
+		<div class="col-md-1 sociallink_outer">
+	<img src="{{URL::asset('img/arrow.png')}}">
+	</div>
+		<div class="col-md-10 sociallink_outertxt">
+	<a href="#">team</a>
+	</div>
+	</div>
+		<div class="col-md-12 sociallink_outer">
+		<hr>
+		<div class="col-md-1 sociallink_outer">
+	<img src="{{URL::asset('img/arrow.png')}}">
+	</div>
+		<div class="col-md-10 sociallink_outertxt">
+	<a href="#">Privecy Policy</a>
+	</div>
+	</div>
+			<div class="col-md-12 sociallink_outer">
+			<hr>
+	</div>
+		</div>
+		</div>
+		</div>
+		</div>
+		</div>
+			<div class="footer_botouter">
+		<div class="container">
+			<div class="row">
+			<div class="col-md-12 footer_botinner">
+		<h5>Copyright 2015 VKULP | All Right Preserved</h5>
+			</div>
+			</div>
+			</div>
+			</div>
+</body>
+ <script src="{{URL::asset('js/bootstrap.js')}}"></script>
+<script src="{{URL::asset('js/jquery.js')}}"></script>
+</html>
