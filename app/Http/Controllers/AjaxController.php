ab9462e9a246fdd6fd9b9fa94e9a295f793ea99f
Nelabh Kotiya <nelabhkotiya@gmail.com>
2016-01-14 23:52:32 +0530

    Ajax Controller Added

---
+++ app/Http/Controllers/AjaxController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+
+class AjaxController extends BaseController
+{
+    use DispatchesJobs, ValidatesRequests;
+
+
+}
